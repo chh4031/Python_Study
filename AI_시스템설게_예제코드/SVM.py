05f90ee948f8c7ab5447ff3c1666d946c849bca0
chh4031 <chh4031@gmail.com>
2023-05-06 15:28:25 +0900

    AIS008

---
+++ AI_시스템설게_예제코드/SVM.py
@@ -0,0 +1,11 @@
+from sklearn import svm
+from sklearn import datasets
+
+d=datasets.load_iris()
+s = svm.SVC(gamma=0.1, C=10)
+s.fit(d.data, d.target)
+
+new_d=[[6.4,3.2,6.0,2.5],[7.1,3.1,4.7,1.35]]
+
+res=s.predict(new_d)
+print("새로운 2개의 샘플의 부류는", res)
