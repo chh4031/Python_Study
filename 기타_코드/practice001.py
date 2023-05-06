d85e784c11597821dd59d91c35e99828f562faf6
chh4031 <chh4031@gmail.com>
2023-05-06 15:21:24 +0900

    ETC005

---
+++ 기타_코드/practice001.py
@@ -0,0 +1,15 @@
+a = str(472)
+b = str(385)
+c = [i for i in b]
+count = str(0)
+result = 0
+
+d = []
+for i in b:
+    d.append(int(a)*int(i))
+d.reverse()
+for k in range(len(d)):
+    d[k] = int(str(d[k]) + count*k)
+    print(d[k])
+    result += d[k]
+print(result)
