efbf8fe6f2e4e42523c60fc5b6bd989b9f6038f8
chh4031 <chh4031@gmail.com>
2023-05-08 21:19:42 +0900

    EX135

---
+++ 코딩테스트/Coding_Test_EX135.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    even = ""
+    odd = ""
+    for i in num_list:
+        if i % 2 == 0:
+            even += str(i)
+        else:
+            odd += str(i)
+    return int(even) + int(odd)
+print(solution([3, 4, 5, 2, 1]))
