adf251cb033ecfc070e1b76773379ba45730a8a6
chh4031 <chh4031@gmail.com>
2023-05-15 18:02:21 +0900

    EX161

---
+++ 코딩테스트/Coding_Test_EX161.py
@@ -0,0 +1,15 @@
+def solution(arr1, arr2):
+    if len(arr1) < len(arr2):
+        return -1
+    elif len(arr1) > len(arr2):
+        return 1
+    else:
+        if sum(arr1) < sum(arr2):
+            return -1
+        elif sum(arr1) > sum(arr2):
+            return 1
+        else:
+            return 0
+
+
+print(solution([49, 13], [70, 11, 2]))
